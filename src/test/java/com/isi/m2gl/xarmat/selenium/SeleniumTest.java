d7d1d0a8f869772d34e83e93ca86a8682be3bdc0
Mbaye Sokhna THIAM <mbayesokhnathiam@gmail.com>
2019-12-07 18:01:05 +0000

    Test commented

--- src/test/java/com/isi/m2gl/xarmat/selenium/SeleniumTest.java
+++ src/test/java/com/isi/m2gl/xarmat/selenium/SeleniumTest.java
@@ -10,24 +10,24 @@ public class SeleniumTest {
 
     @Test
     public void test1() throws InterruptedException{
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\M.Thiam\\Documents\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.facebook.com");
-        driver.manage().window().maximize();
-
-        WebElement email = driver.findElement(By.xpath("//input[@data-testid='royal_email']"));
-        WebElement pass = driver.findElement(By.xpath("//input[@data-testid='royal_pass']"));
-        WebElement btn = driver.findElement(By.xpath("//input[@data-testid='royal_login_button']"));
-        email.sendKeys("babsdaboss");
-        pass.sendKeys("password");
-
-        btn.click();
-
-        WebElement search = driver.findElement(By.id("fi-g"));
-        search.sendKeys("camera");
-
-        WebElement searchIcon = driver.findElement(By.xpath("//button[@class='btn _prim _md -mls -fsh0']"));
-        searchIcon.click();
-        Thread.sleep(5000);
+//        System.setProperty("webdriver.chrome.driver", "C:\\Users\\M.Thiam\\Documents\\chromedriver.exe");
+//        WebDriver driver = new ChromeDriver();
+//        driver.get("https://www.facebook.com");
+//        driver.manage().window().maximize();
+//
+//        WebElement email = driver.findElement(By.xpath("//input[@data-testid='royal_email']"));
+//        WebElement pass = driver.findElement(By.xpath("//input[@data-testid='royal_pass']"));
+//        WebElement btn = driver.findElement(By.xpath("//input[@data-testid='royal_login_button']"));
+//        email.sendKeys("babsdaboss");
+//        pass.sendKeys("password");
+//
+//        btn.click();
+//
+//        WebElement search = driver.findElement(By.id("fi-g"));
+//        search.sendKeys("camera");
+//
+//        WebElement searchIcon = driver.findElement(By.xpath("//button[@class='btn _prim _md -mls -fsh0']"));
+//        searchIcon.click();
+//        Thread.sleep(5000);
     }
 }
