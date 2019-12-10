bf5a6ec2405d17fec08ef93cff715736cea15db9
Mouhamed NDOYE <ndoyeahmed2602@gmail.com>
2019-12-10 20:22:36 +0000

    integration test

--- src/test/java/com/isi/m2gl/xarmat/selenium/Selenium.java
+++ src/test/java/com/isi/m2gl/xarmat/selenium/Selenium.java
@@ -1,24 +1,22 @@
 package com.isi.m2gl.xarmat.selenium;
 
 import com.isi.m2gl.xarmat.XarmatApp;
-import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.io.File;
-import java.io.IOException;
 
 @SpringBootTest(classes = XarmatApp.class)
 public class Selenium {
 
     @Test
     public void test1() throws InterruptedException {
-//        System.setProperty("webdriver.gecko.driver", "D:\\cours\\M2\\devOps\\geckodriver\\geckodriver.exe");
-        System.setProperty("webdriver.chrome.driver", "D:\\cours\\M2\\devOps\\chromedriver_win32\\chromedriver.exe");
-//        WebDriver driver = new FirefoxDriver();
-        WebDriver driver = new ChromeDriver();
+        System.setProperty("webdriver.gecko.driver", "D:\\cours\\M2\\devOps\\geckodriver\\geckodriver.exe");
+//        System.setProperty("webdriver.chrome.driver", "D:\\cours\\M2\\devOps\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver = new FirefoxDriver();
+//        WebDriver driver = new ChromeDriver();
         driver.get("http://localhost:8082/xarmat/");
         driver.manage().window().maximize();
 
@@ -37,7 +35,7 @@ public class Selenium {
         WebElement signin = driver.findElement(By.xpath("//button[@class='btn btn-primary']"));
         signin.click();
 
-        WebElement search = driver.findElement(By.id("fi-q"));
+        /*WebElement search = driver.findElement(By.id("fi-q"));
         search.sendKeys("camera");
 
         TakesScreenshot ts = (TakesScreenshot) driver;
@@ -46,7 +44,7 @@ public class Selenium {
             FileUtils.copyFile(source, new File("screenmouhamed.png"));
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
 
 //        WebElement searchIcon = driver.findElement(By.xpath("//button[@class='btn _prim _md -mls -fsh0']"));
 //        searchIcon.click();
