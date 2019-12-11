677aefc924eb3318c845ca603d3f96d295af3af7
ABDOULBASSUR ABDOU SOIMADOU <a.soimadou2013@gmail.com>
2019-12-11 18:38:52 +0000

    modification selenium

---
+++ src/test/java/com/isi/m2gl/xarmat/selenium/SeleniumTest.java
@@ -0,0 +1,38 @@
+package com.isi.m2gl.xarmat.selenium;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SeleniumTest {
+	@Test
+	public void TestConnexion() {
+		try {
+
+			System.out.println("Hello World!");
+			System.setProperty("webdriver.gecko.driver", "D:\\geckodriver.exe");
+			WebDriver driver = new FirefoxDriver();
+			driver.get("http://localhost:9091/xarmat/");
+			driver.manage().window().maximize();
+			WebElement sbb = driver.findElement(By.id("account-menu"));
+			sbb.click();
+			Thread.sleep(2000);
+			sbb = driver.findElement(By.id("login"));
+			sbb.click();
+			sbb = driver.findElement(By.id("username"));
+			sbb.sendKeys("admin");
+			sbb = driver.findElement(By.id("password"));
+			sbb.sendKeys("admin");
+			sbb = driver.findElement(By.className("btn"));
+			Thread.sleep(5000);
+			sbb.click();
+			Thread.sleep(5000);
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+}
