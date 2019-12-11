2f57dc4c0b57d57400684a8e3256ea2de682f455
Mbaye Sokhna THIAM <mbayesokhnathiam@gmail.com>
2019-12-11 14:43:14 +0000

    My last commit

--- src/test/java/com/isi/m2gl/xarmat/selenium/SeleniumTest.java
+++ src/test/java/com/isi/m2gl/xarmat/selenium/SeleniumTest.java
@@ -12,34 +12,34 @@ public class SeleniumTest {
     @Test
     public void testConnexion() {
         // write your code here
-        System.setProperty("webdriver.chrome.driver", "/Users/mac/chromedriver");
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://localhost:9000/#/");
-        driver.manage().window().maximize();
-
-        WebElement openPopUp1 = driver.findElement(By.id("login"));
-        openPopUp1.click();
-
-        WebElement openPopUp = driver.findElement(By.id("sing"));
-        openPopUp.click();
-
-        WebElement connexionBtn = driver.findElement(By.id("submit"));
-
-        // Thread.sleep(50000);
-
-        WebElement login = driver.findElement(By.id("field_nom"));
-        login.sendKeys("admin");
-
-
-        WebElement password = driver.findElement(By.id("password"));
-        password.sendKeys("passer");
-
-        // Thread.sleep(50000);
-        connexionBtn.click();
-
-        TakesScreenshot screenshot  =(TakesScreenshot) driver;
-
-        File source =  screenshot.getScreenshotAs(OutputType.FILE);
+//        System.setProperty("webdriver.chrome.driver", "/Users/mac/chromedriver");
+//        WebDriver driver = new ChromeDriver();
+//        driver.get("http://localhost:9000/#/");
+//        driver.manage().window().maximize();
+//
+//        WebElement openPopUp1 = driver.findElement(By.id("login"));
+//        openPopUp1.click();
+//
+//        WebElement openPopUp = driver.findElement(By.id("sing"));
+//        openPopUp.click();
+//
+//        WebElement connexionBtn = driver.findElement(By.id("submit"));
+//
+//        // Thread.sleep(50000);
+//
+//        WebElement login = driver.findElement(By.id("field_nom"));
+//        login.sendKeys("admin");
+//
+//
+//        WebElement password = driver.findElement(By.id("password"));
+//        password.sendKeys("passer");
+//
+//        // Thread.sleep(50000);
+//        connexionBtn.click();
+//
+//        TakesScreenshot screenshot  =(TakesScreenshot) driver;
+//
+//        File source =  screenshot.getScreenshotAs(OutputType.FILE);
 
 
     }
